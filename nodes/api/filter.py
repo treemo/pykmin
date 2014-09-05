edc914cb7b9f71fd997fc0fe60716d7496bac4fa
Yoann Ono Dit Biot <y0no@y0no.fr>
2014-09-05 23:12:12 +0200

    Fixes API filter issue.

--- nodes/api/filter.py
+++ nodes/api/filter.py
@@ -5,26 +5,25 @@ from core.helpers.filters import BaseFilter
 class OrderHandler(BaseFilter):
 
     def filter(self, data):
-        data = data.decode('utf-8')
-        order = data.rstrip().split(maxsplit=1)
+        order = data.split(maxsplit=1)
         if order[0] == 'stop':
             module = order[1]
             module_instance = inputs.get_element(module)
             module_instance.stop()
-            return b'Module sucessfully stopped'
+            return 'Module sucessfully stopped'
 
         if order[0] == 'start':
             module = order[1]
             module_instance = inputs.get_element(module)
             module_instance.start()
-            return b'Module sucessfully started'
+            return 'Module sucessfully started'
 
         if order[0] == 'list':
             ret = []
             for element in inputs.get_all_elements():
                 ret.append("- %s" % element)
-            return '\n'.join(ret).encode('utf-8')
+            return '\n'.join(ret)
 
-        return b'Error ...'
+        return 'Error ...'
 
 filters.register('pykmin_api_filter', OrderHandler)
